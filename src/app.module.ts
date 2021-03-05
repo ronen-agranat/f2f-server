0e78de7b8206595dc56fe6637ccb8e714c82d818
Ronen Agranat <ronen.agranat@gmail.com>
2021-03-05 21:39:52 +0100

    WIP: Adding auth with @nestjs/passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonsModule } from './persons/persons.module';
 import { MinutesModule } from './minutes/minutes.module';
 import { DatabaseConfig } from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     PersonsModule,
     MinutesModule,
-    TypeOrmModule.forRoot(DatabaseConfig)
+    TypeOrmModule.forRoot(DatabaseConfig),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
